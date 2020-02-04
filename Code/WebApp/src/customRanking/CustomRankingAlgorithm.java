7dfb22ac1e706b2c002d3913d42b49c9df45e746
Tom <shmiffeh@gmail.com>
2020-02-04 13:13:36 +0000

    Created test for sigmoid function
    Wrote a test to check if a sigmoid function from CustomRankingAlgorithm returns the expected result

---
+++ Code/WebApp/src/customRanking/CustomRankingAlgorithm.java
@@ -0,0 +1,19 @@
+package customRanking;
+
+public class CustomRankingAlgorithm {
+
+	final float e = 2.71828f;
+	
+	
+	public static float sigmoidFunction(int x)
+	{
+		float y = 0.0f;
+		
+		
+		
+		
+		return y;
+	}
+	
+	
+}
---
+++ Code/WebApp/src/customRanking/CustomRankingAlgorithmTest.java
@@ -0,0 +1,29 @@
+package customRanking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomRankingAlgorithmTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void sigmoidFunctionTest() {
+		float result = 0.0f;
+		result = CustomRankingAlgorithm.sigmoidFunction(3);
+		assertEquals(0.049787f, result, "Sigmoid function did not calculate y correctly");
+		
+		result = CustomRankingAlgorithm.sigmoidFunction(20);
+		assertNotEquals(0.3f, result, "Sigmoid function returned a correct result of y that it was not meant to");
+	}
+
+}
