85df3f63c30bd912ad50fd5d3aa48f2281810508
adidude <menon.or.adithya@gmail.com>
2020-02-04 11:02:22 +0000

    Map now can show hospital locations
    TODO: REplace icon on map

--- Code/WebApp/WebContent/map.js
+++ Code/WebApp/WebContent/map.js
@@ -1,9 +1,9 @@
-var platform = new H.service.Platform({
+var api = new H.service.Platform({
   'apikey': '{xzADpXW7FDuY_UZUfXACnvhd1mlSWTHJf1oo4-Q1h50}'
 });
 
 // Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
+var defaultLayers = api.createDefaultLayers();
 
 // Instantiate (and display) a map object:
 var map = new H.Map(
@@ -13,3 +13,19 @@ var map = new H.Map(
       zoom: 3.6,
       center: { lat: 39, lng: -104 }
     });
+
+    var svgMarkup = '<svg width="24" height="24" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+        'height="22" /><text x="12" y="18" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white">H</text></svg>';
+
+    // Create an icon, an object holding the latitude and longitude, and a marker:
+    var icon = new H.map.Icon(svgMarkup),
+        coords = {lat: 39, lng: -104},
+        marker = new H.map.Marker(coords, {icon: icon});
+
+    // Add the marker to the map and center the map at the location of the marker:
+    map.addObject(marker);
+    map.setCenter(coords);
