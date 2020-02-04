decfc54440b23ccad1ffccfe6d90ee42725ed7c4
adidude <menon.or.adithya@gmail.com>
2020-02-04 22:02:48 +0000

    Added partial map functionality
    This includes:
    -Zoom in/out UI
    -Layers view menu
    -Info Bubble

--- Code/WebApp/WebContent/map.js
+++ Code/WebApp/WebContent/map.js
@@ -12,20 +12,34 @@ var map = new H.Map(
     {
       zoom: 3.6,
       center: { lat: 39, lng: -104 }
-    });
+    }
+);
 
-    var svgMarkup = '<svg width="24" height="24" ' +
+var svgMarkup = '<svg width="24" height="24" ' +
         'xmlns="http://www.w3.org/2000/svg">' +
         '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
         'height="22" /><text x="12" y="18" font-size="12pt" ' +
         'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
         'fill="white">H</text></svg>';
 
-    // Create an icon, an object holding the latitude and longitude, and a marker:
-    var icon = new H.map.Icon(svgMarkup),
-        coords = {lat: 39, lng: -104},
-        marker = new H.map.Marker(coords, {icon: icon});
+// Create an icon, an object holding the latitude and longitude, and a marker:
+var icon = new H.map.Icon(svgMarkup),
+    coords = {lat: 39, lng: -104},
+    marker = new H.map.Marker(coords, {icon: icon});
 
-    // Add the marker to the map and center the map at the location of the marker:
-    map.addObject(marker);
-    map.setCenter(coords);
+  // Add the marker to the map and center the map at the location of the marker:
+  map.addObject(marker);
+
+var mapEvents = new H.mapevents.MapEvents(map);
+
+var ui = H.ui.UI.createDefault(map, defaultLayers, 'en-US');
+
+var bubble = new H.ui.InfoBubble({lat: 39, lng: -103},{
+  content:'<p>Hospital</p>'
+});
+
+ui.addBubble(bubble);
+
+map.addEventListener('drag',function(evt){
+  alert(evt);
+});
